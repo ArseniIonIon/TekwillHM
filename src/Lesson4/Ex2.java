1393131369580942210e9774a9f7b32d5946f8d8
iarseni <Ion.Arseni@endava.com>
2020-07-01 16:39:51 +0300

    Added lesson 4 Home Work

---
+++ src/Lesson4/Ex2.java
@@ -0,0 +1,9 @@
+package Lesson4;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        int  a = 512343;
+        int  b = 3431231;
+        System.out.println(a + b);
+    }
+}
---
+++ src/Lesson4/Ex3.java
@@ -0,0 +1,8 @@
+package Lesson4;
+
+public class Ex3 {
+    public static void main(String[] args) {
+        int variable = 123456;
+        System.out.println(variable);
+    }
+}
