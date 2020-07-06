f0aecaa98d78efd437c8ee68c6ca290847fbcf07
iarseni <Ion.Arseni@endava.com>
2020-07-06 16:14:42 +0300

    added Ex8

--- src/Lesson5/Ex8.java
+++ src/Lesson5/Ex8.java
@@ -7,9 +7,23 @@ public class Ex8 {
     public static void main(String[] args) {
 
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Enter some numbers between 1 and 8.");
-        String input = userInput.nextLine();
-        System.out.println(input.split(" ").length); //Count inputs
-        userInput.close();
+        printWordsOfLines(userInput);
+    }
+
+    public static void printWordsOfLines(Scanner x){
+        System.out.println("Enter 0 to quite input");
+        int number=0;
+
+        while(x.hasNextInt()){
+            int xx = x.nextInt();
+
+            if(xx == 0)
+                break;
+            else
+                number = number+1;
+
+        }
+        System.out.println("numers: " + number);
+
     }
 }
