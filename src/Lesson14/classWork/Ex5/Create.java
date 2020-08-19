95e5a3b37263059a11079052d79c88a3aed87f0a
ArseniIonIon <arseniion2@gmail.com>
2020-08-19 18:56:23 +0300

    Merge branch 'master' of /Users/mymac/IdeaProjects/TekwillHM with conflicts.

--- src/Lesson14/classWork/Ex5/Create.java
+++ src/Lesson14/classWork/Ex5/Create.java
@@ -1,13 +1,14 @@
 package Lesson14.classWork.Ex5;
 
-public class Create{
+public class Create
+{
 
     public static Runnable createRunnable(String text, int repeat){
         return new Runnable() {
             @Override
             public void run() {
                 for (int i = 0; i < repeat; i++) {
-                    System.out.println(text);
+                   // System.out.println(text);
                 }
             }
         };
